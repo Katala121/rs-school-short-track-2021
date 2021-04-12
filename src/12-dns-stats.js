27fa1759bc171c095de1677288a452fb755d672f
Katala121 <sanyafootb@list.ru>
2021-04-13 00:59:28 +0300

    feat: implement fanctions 13 and 15

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,31 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const dnsStats = {};
+  const domainsParse = domains.map((item) => item.split('.'));
+  console.log(domainsParse);
+  dnsStats[`.${domainsParse[0][domainsParse[0].length - 1]}`] = domainsParse.reduce((count, item) => {
+    let i;
+    if (item[item.length - 1] === domainsParse[0][domainsParse[0].length - 1]) i = 1;
+    return count + i;
+  }, 0);
+  dnsStats[`.${domainsParse[0][domainsParse[0].length - 1]}.${domainsParse[0][domainsParse[0].length - 2]}`] = domainsParse.reduce((count, item) => {
+    let i;
+    if (item[item.length - 2] === domainsParse[0][domainsParse[0].length - 2]) i = 1;
+    return count + i;
+  }, 0);
+  if (domainsParse[1] && domainsParse[1].length === 3) {
+    dnsStats[`.${domainsParse[1][2]}.${domainsParse[1][1]}.${domainsParse[1][0]}`] = domainsParse.reduce((count, item) => {
+      let i = 0;
+      if (item.length === 3) {
+        i = 1;
+        console.log(count);
+      }
+      return count + i;
+    }, 0);
+  }
+  return dnsStats;
 }
 
 module.exports = getDNSStats;
